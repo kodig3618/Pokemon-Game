323cc3a141e8bc2511e111e84d991435fbe21773
Kodi <kodigolay3618@gmail.com>
2025-08-15 15:37:10 -0500

    moving

--- index.js
+++ index.js
@@ -13,9 +13,98 @@ image.src = './img/Town.png';
 const playerImage = new Image();
 playerImage.src = './img/playerDown.png';
 
-image.onload = () => {
-    ctx.drawImage(image, -200, -50);
-    ctx.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2 ,
-         canvas.height / 2 - playerImage.height / 2);
+class Sprite {
+    constructor({ position, velocity, image }) {
+        this.position = position
+        this.image = image
+    }
+
+    draw() {
+        ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+const background = new Sprite({position: {
+    x: -200,
+    y: -50,
+    },
+    image: image
+})
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    background.draw();
+    ctx.drawImage(
+        playerImage, 
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
+
+    if (keys.w.pressed) background.position.y += 3;
+    else if (keys.a.pressed) background.position.x += 3;
+    else if (keys.s.pressed) background.position.y -= 3;
+    else if (keys.d.pressed) background.position.x -= 3;
     
 }
+animate();
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true;
+            break;
+
+        case 'a':
+            keys.a.pressed = true;
+            break;
+
+        case 's':
+            keys.s.pressed = true;
+            break;
+
+        case 'd':
+            keys.d.pressed = true;
+            break;
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+
+        case 'a':
+            keys.a.pressed = false;
+            break;
+
+        case 's':
+            keys.s.pressed = false;
+            break;
+            
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
+})
