9135e1e8f99d168bf8330cf6a820efa3ed8ed7da
Kodi <kodigolay3618@gmail.com>
2025-08-15 10:48:39 -0500

    imgs

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
+image.src = './img/Town.png';
+
+const playerImage = new Image();
+playerImage.src = './img/playerDown.png';
 
 image.onload = () => {
-    ctx.drawImage(image, 0, 0);
+    ctx.drawImage(image, -200, -50);
+    ctx.drawImage(playerImage, canvas.width / 2 - playerImage.width / 2 ,
+         canvas.height / 2 - playerImage.height / 2);
+    
 }
-
-image.src = './img/Town.png';
